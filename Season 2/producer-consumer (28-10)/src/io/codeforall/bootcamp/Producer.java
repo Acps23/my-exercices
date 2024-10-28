fa1bbabe9d844d241020387def950599cd02e32b
Cristina Silva <acps.cristina@gmail.com>
2024-10-28 15:52:55 +0000

    adds producer consumer

---
+++ Season 2/producer-consumer (28-10)/src/io/codeforall/bootcamp/Producer.java
@@ -0,0 +1,35 @@
+package io.codeforall.bootcamp;
+
+import io.codeforall.bootcamp.bqueue.BQueue;
+import io.codeforall.bootcamp.bqueue.Pizza;
+
+/**
+ * Produces and stores integers into a blocking queue
+ */
+public class Producer implements Runnable {
+
+    private final BQueue<Pizza> queue;
+    private int elementNum;
+
+    /**
+     * @param queue      the blocking queue to add elements to
+     * @param elementNum the number of elements to produce
+     */
+    public Producer(BQueue queue, int elementNum) {
+        this.queue = queue;
+        this.elementNum = elementNum;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < elementNum; i++) {
+            try {
+                queue.offer(new Pizza());
+
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+}
